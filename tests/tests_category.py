aa94ee1436e869826f9326a10aa515c72c128319
pndemo <ndemopaul1@gmail.com>
2017-11-26 23:25:32 +0300

    [UPDATE] TestCases remodelled

--- tests/tests_category.py
+++ tests/tests_category.py
@@ -1,53 +1,143 @@
 """ Category module tests """
 
 import unittest
-from app import app
+from app import app, users, categories
 
 class CategoryTests(unittest.TestCase):
     """ Category tests """
 
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-        self.register = self.app.post('/register', data=dict(username='ExampleUser', email=\
-                'example@domain.com', password='Bootcamp17', confirm_password='Bootcamp17'), \
-                follow_redirects=True)
-        self.login = self.app.post('/login', data=dict(email='example@domain.com', password=\
-                'Bootcamp17'), follow_redirects=True)
-        self.create_category = self.app.post('/create_category', data=dict(category_name= \
-                'Breakfast'), follow_redirects=True)
+        app.testing = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        with app.app_context():
+            app.test_client().get('/logout')
+            del users[:]
+            del categories[:]
+        self.client = app.test_client
+        self.register_data = {'username': 'ExampleUser', 'email': 'example@domain.com', \
+                'password': 'Bootcamp17', 'confirm_password': 'Bootcamp17'}
+        self.login_data = {'username': 'ExampleUser', 'password': 'Bootcamp17'}
+        self.category_data = {'category_name': 'Breakfast'}
 
-    def test_create_category(self):
+    def test_create_successful(self):
         """ Test for successful category creation """
-        response = self.register
-        self.assertEqual(response.status_code, 200)
-        response = self.login
-        self.assertEqual(response.status_code, 200)
-        response = self.create_category
-        self.assertEqual(response.status_code, 200)
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
 
-    def test_update_category(self):
+    def test_create_empty_field(self):
+        """ Test for unsuccessful category creation with empty field """
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        self.category_data['category_name'] = ''
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please enter category name', response.data)
+
+    def test_create_invalid_name(self):
+        """ Test for unsuccessful category creation with invalid category name """
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        self.category_data['category_name'] = 'Break#@^&'
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please enter a valid category name', response.data)
+
+    def test_create_registered_name(self):
+        """ Test for unsuccessful category creation with already registered category name """
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'A category with this category name is already available', response.data)
+
+    def test_get_categories(self):
+        """ Test for display of created categories """
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().get('/home')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Breakfast', response.data)
+
+    def test_update_successful(self):
         """ Test for successful category update """
-        response = self.register
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        self.category_data['category_name'] = 'Breakfast2'
+        response = self.client().post('/update_category?category_id=1', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_update_empty_field(self):
+        """ Test for unsuccessful category update with empty field """
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        self.category_data['category_name'] = ''
+        response = self.client().post('/update_category?category_id=1', data=self.category_data)
         self.assertEqual(response.status_code, 200)
-        response = self.login
+        self.assertIn(b'Please enter category name', response.data)
+
+    def test_update_invalid_name(self):
+        """ Test for unsuccessful category update with invalid category name """
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        self.category_data['category_name'] = 'Break#@^&'
+        response = self.client().post('/update_category?category_id=1', data=self.category_data)
         self.assertEqual(response.status_code, 200)
-        response = self.create_category
+        self.assertIn(b'Please enter a valid category name', response.data)
+
+    def test_update_registered_name(self):
+        """ Test for unsuccessful category update with already registered category name """
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        self.category_data['category_name'] = 'Breakfast2'
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        self.category_data['category_name'] = 'Breakfast'
+        response = self.client().post('/update_category?category_id=2', data=self.category_data)
         self.assertEqual(response.status_code, 200)
-        response = self.app.post('/update_category?category_id=1', data=dict(category_name= \
-                'Lunch'), follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'A category with this category name is already available', response.data)
 
     def test_delete_category(self):
         """ Test for successful category deletion """
-        response = self.register
-        self.assertEqual(response.status_code, 200)
-        response = self.login
-        self.assertEqual(response.status_code, 200)
-        response = self.create_category
-        self.assertEqual(response.status_code, 200)
-        response = self.app.post('/delete_category?category_id=1', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        response = self.client().post('/register', data=self.register_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/login', data=self.login_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/create_category', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
+        response = self.client().post('/delete_category?category_id=1', data=self.category_data)
+        self.assertEqual(response.status_code, 302)
 
 if __name__ == '__main__':
     unittest.main()
